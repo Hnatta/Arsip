7446de422cb1f4d5a78abfe6e7e3e4fef0049142
Hnatta <66830227+Hnatta@users.noreply.github.com>
2023-10-13 18:24:28 +0700

    Update note.js

--- note.js
+++ note.js
@@ -1,4 +1,7 @@
-[{note: "Cara terbaik tuk melupakan masa lalu adalah bukan dengan menghindari atau menyesalinya, namun dengan menerima dan memaafkanya.",},
+  const data = [
+
+    
+ {note: "Cara terbaik tuk melupakan masa lalu adalah bukan dengan menghindari atau menyesalinya, namun dengan menerima dan memaafkanya.",},
 
   {note:"Jika kita biarkan masa lalu mengusik hidup kita saat ini, tentu kita tak akan pernah bisa berikan yg terbaik tuk dapat masa depan yg baik.",},
 
@@ -34,3 +37,45 @@
 
   {note:"Taukah kau aku bisa jadi pelangi untukmu tapi aku juga bisa jadi hujan bahkan badai sekalipun jika kau sakiti .",  },
 ];
+
+const showMoreBtn = document.querySelector(
+  ".testimonials-container .show-more-btn"
+);
+const testimonials = document.querySelector(".testimonials");
+let latestTestimonialIndex = 0;
+
+const generateTestimonial = (videoUrl, review, name, designation) => {
+  const htmlCode = `
+<div class="testimonial">
+  <div class="note"  style="position: relative;border: 1px solid #dddddd;-moz-border-radius: 4px;border-radius: 4px;padding: 15px 30px;margin: 15px 0px;font-family: &quot;Roboto&quot;, &quot;Helvetica Neue&quot;, Helvetica, Arial, sans-serif;font-weight: 400;text-align: left;display: flex;text-decoration: none;text-transform: none;color: #666666;cursor: pointer;font-size: 24px;line-height: 140%;letter-spacing: 0.05em;justify-content: center;">
+    ${review}
+  </div>
+  <div class="bottom">
+</div>`;
+
+  return htmlCode;
+};
+
+const showTestimonial = () => {
+  let index = latestTestimonialIndex;
+
+  for (let i = index; i < index + 4; i++) {
+    if (!data[i]) {
+      showMoreBtn.style.display = "none";
+      return;
+    }
+
+    testimonials.innerHTML += generateTestimonial(
+      data[i].videoUrl,
+      data[i].note,
+      data[i].name,
+      data[i].designation
+    );
+
+    latestTestimonialIndex++;
+  }
+};
+
+showTestimonial();
+
+showMoreBtn.addEventListener("click", showTestimonial);
